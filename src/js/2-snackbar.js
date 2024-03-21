86468c3594fe54ce1a76e7f540798701a5779f91
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-03-21 23:21:50 +0200

    update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,31 +6,29 @@ import 'izitoast/dist/css/iziToast.min.css';
 const submitForm = document.querySelector('.form');
 
 submitForm.addEventListener('submit', event => {
-    event.preventDefault();
-    
+  event.preventDefault();
 
-    const delayInput = document.querySelector('input[name = "delay"]');
-    const delay = delayInput.value;
-    const state = document.querySelector('input[name = "state"]:checked').value;
+  const delayInput = document.querySelector('input[name = "delay"]');
+  const delay = delayInput.value;
+  const state = document.querySelector('input[name = "state"]:checked').value;
 
-    const message = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (state === 'fulfilled') {
-                resolve(delay);
-            } else {
-                reject(delay);
-            }
-        }, delay);
+  const message = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  message.then(delay => {
+    console.log(`✅ Fulfilled promise in ${delay}ms`);
+    iziToast.success({
+      title: 'OK',
+      message: 'Illegal operation',
+      position: 'topRight',
     });
-
-    message.then(delay => {
-        console.log(`✅ Fulfilled promise in ${delay}ms`);
-        iziToast.success({
-            title: 'OK',
-            message: 'Illegal operation',
-            position: 'topRight',
-
-        });
-    });
-    submitForm.();
+  });
+  submitForm.reset();
 });
