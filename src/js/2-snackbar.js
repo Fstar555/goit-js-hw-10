e24bb6014c6cf2a933af28771f454f90598d1164
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-03-21 23:12:08 +0200

    update snackbar.js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const submitForm = document.querySelector('.form');
+
+submitForm.addEventListener('submit', event => {
+    event.preventDefault();
+    
+
+    const delayInput = document.querySelector('input[name = "delay"]');
+    const delay = delayInput.value;
+    const state = document.querySelector('input[name = "state"]:checked').value;
+
+    const message = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    message.then(delay => {
+        console.lo(`✅ Fulfilled promise in ${delay}ms`);
+        iziToast.success({
+            title: 'OK',
+            message: 'Illegal operation',
+            position: 'topRight',
+
+        });
+    });
+    submitForm.();
+});
